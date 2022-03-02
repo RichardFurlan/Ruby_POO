2b7e063caefc2c6787d2596e0f68c5600e2f442a
Richard Figueiredo Furlan <77584337+RichardFurlan@users.noreply.github.com>
2022-03-01 22:02:24 -0300

    Add files via upload

---
+++ turma/turma.rb
@@ -0,0 +1,20 @@
+class Turma
+    attr_reader :alunos, :professor
+
+    def initialize(alunos, professor)
+        @alunos = Array.new  #Poderiamos colcoar apenas []
+        @professor = professor
+    end
+
+    def dados_turma
+        puts "Disciplina: #{professor.disciplina}, Professor: #{professor.nome}"
+        lista_alunos
+    end
+
+    private
+    def lista_alunos
+        @alunos.each do |aluno|
+        puts "Nome: #{aluno.nome}, Matricula: #{aluno.matricula}"
+        end
+    end
+end
